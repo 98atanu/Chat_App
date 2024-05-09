f807d10f67c736cd324d5b38325ff033b3b11036
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-05-10 00:13:53 +0530

    Loading and chat ui fixed.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,15 +3,21 @@ import { OpenRouter_ai } from "../network/OpenRouter_ai";
 import ChatCard from "./ChatCard";
 import { IoToggle } from "react-icons/io5";
 import { FaTelegram } from "react-icons/fa6";
+import { SyncLoader } from "react-spinners";
+
 
 const Chat = () => {
     const [value, setValue] = useState("");
     const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false)
   
 
     const handleSubmit = useCallback(
         async (value) => {
+            if (!value) return;
             setMessages((prev) => [...prev, { sender: "user", message: value }]);
+            setLoading(true)
+            setValue(" ")
 
             try {
                 const res = await OpenRouter_ai(value);
@@ -21,9 +27,10 @@ const Chat = () => {
             } catch (error) {
                 console.error("An error occurred", error);
             }
-            setValue("")
+            setLoading(false)
+           
         },
-        [setMessages,setValue]
+        [setMessages]
     );
 
     const handleKeyPress = (event) => {
@@ -31,32 +38,38 @@ const Chat = () => {
             handleSubmit(value)
            
         }
-
-
     }
 
     return (
         <div className="relative w-full h-screen">
+             
             {messages.map((data, index) => (
-                <ChatCard key={index} data={data.message} />
+                <ChatCard key={index} data={data.message} sender={data.sender} />
             ))}
+            {loading && (
+            <div  className="absolute left-8  mt-3 "><SyncLoader color="#d62b5e" size={10} /></div>
+        )}
             <div className="absolute bottom-10 left-40 flex gap-3 items-center ">
                 <div className="flex flex-col items-end">
                     <button className="flex gap-1 items-center text-[#d62b5e] text-sm">
                         text <IoToggle className="text-[#d62b5e] "/>
                     </button>
                     <input
-                        className=" border-[2px] border-[#b00909] px-2 py-1 rounded-md bg-[#121212] w-[40vw]"
+                        className=" border-[2px] border-[#b00909] px-2 py-1 rounded-md bg-[#121212] w-[40vw] outline-none"
+                        value={value}
                         onChange={(e) => setValue(e.target.value)}
                         onKeyDown={handleKeyPress}
+
                         placeholder="Type a message here..."
                     />
                 </div>
+               
                 <div>
-                    <button className="text-3xl translate-y-4" onClick={() => handleSubmit(value)}>
+                    <button className="text-3xl translate-y-4 text-[#d62b5e]" onClick={() => handleSubmit(value)}>
                         <FaTelegram />
                     </button>
                 </div>
+
             </div>
         </div>
     );
--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { CgProfile } from "react-icons/cg";
+import { BiSolidBot } from "react-icons/bi";
 
 
-const ChatCard = ({ data }) => {
+
+const ChatCard = ({ data, sender }) => {
   return (
-    <div className='p-2'>
-      <div className='flex items-center mb-2 gap-2'>
-        <CgProfile className='text-3xl'/>
-        Jessica
+    <div className='px-8 py-2 '>
+        {sender === 'user' ? (
+          <>
+          <div className='flex items-center justify-end mb-2 gap-2'>
+            <CgProfile className='text-2xl text-[#277538]' />
+            <div className='text-[#a4a4a1] font-bold'>You</div>
+            </div>
+            <div className='bg-[#262626] px-2 py-1 rounded-l-md rounded-b-md mr-7'>
+              {data}
+            </div>
+          </>) : (
+          <>
+           <div className='flex items-center mb-2 gap-2'>
+            <BiSolidBot className='text-2xl text-[#cf144ce2]' /><div className=' text-[#a4a4a1] font-bold'>Jessica</div>
+            </div>
+            <div className='bg-[#d62b5e] px-2 py-1 rounded-r-md rounded-b-md ml-7'>
+              {data}
+            </div>
+          </>)}
       </div>
-      <div className='bg-[#d62b5e] px-2 py-1 rounded-md'>
-        {data}
-      </div>
-    </div>
+
   )
 }
 
